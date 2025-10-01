1dc5c88f3382545bee2de2d2d31e04dc1a9cbb1a
Bertrand Martin <bertrmar@shi-r3-p2.s19.be>
2025-10-01 15:21:46 +0200

    travail redir voir pourquoi readline crash

--- src/exec/execve/child.c
+++ src/exec/execve/child.c
@@ -4,5 +4,11 @@ int child(t_shell *shell, t_cmd *cmd)
 {
     (void)shell;
     (void)cmd;
+    if (dup2(cmd->fd_in, STDIN_FILENO))
+        return (1);
+    close (cmd->fd_in);
+    if (dup2(cmd->fd_out, STDOUT_FILENO))
+        return (1);
+    close (cmd->fd_out);
     return (0);
 }
